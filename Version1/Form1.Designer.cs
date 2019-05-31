7f1922100137ce8c69d8b2c66cfdff491a09ee90
LongHT <38778007+longhuynhthanh@users.noreply.github.com>
2019-05-31 21:09:03 +0700

    upload source

---
+++ Version1/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace Version1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tmCar1 = new System.Windows.Forms.Timer(this.components);
+            this.ptrRoad1 = new System.Windows.Forms.PictureBox();
+            this.ptrRoad2 = new System.Windows.Forms.PictureBox();
+            this.ptrRoad3 = new System.Windows.Forms.PictureBox();
+            this.ptrRoad4 = new System.Windows.Forms.PictureBox();
+            this.ptrRoad5 = new System.Windows.Forms.PictureBox();
+            this.ptr1 = new System.Windows.Forms.PictureBox();
+            this.ptr2 = new System.Windows.Forms.PictureBox();
+            this.ptr3 = new System.Windows.Forms.PictureBox();
+            this.ptrPlayer = new System.Windows.Forms.PictureBox();
+            this.tmRoad = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.ptrRoad1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptrRoad2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptrRoad3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptrRoad4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptrRoad5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptr1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptr2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptr3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptrPlayer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tmCar1
+            // 
+            this.tmCar1.Interval = 15;
+            this.tmCar1.Tick += new System.EventHandler(this.tmCar1_Tick);
+            // 
+            // ptrRoad1
+            // 
+            this.ptrRoad1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ptrRoad1.Location = new System.Drawing.Point(31, 151);
+            this.ptrRoad1.Name = "ptrRoad1";
+            this.ptrRoad1.Size = new System.Drawing.Size(100, 52);
+            this.ptrRoad1.TabIndex = 4;
+            this.ptrRoad1.TabStop = false;
+            // 
+            // ptrRoad2
+            // 
+            this.ptrRoad2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ptrRoad2.Location = new System.Drawing.Point(184, 151);
+            this.ptrRoad2.Name = "ptrRoad2";
+            this.ptrRoad2.Size = new System.Drawing.Size(100, 52);
+            this.ptrRoad2.TabIndex = 5;
+            this.ptrRoad2.TabStop = false;
+            // 
+            // ptrRoad3
+            // 
+            this.ptrRoad3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ptrRoad3.Location = new System.Drawing.Point(334, 151);
+            this.ptrRoad3.Name = "ptrRoad3";
+            this.ptrRoad3.Size = new System.Drawing.Size(100, 52);
+            this.ptrRoad3.TabIndex = 6;
+            this.ptrRoad3.TabStop = false;
+            // 
+            // ptrRoad4
+            // 
+            this.ptrRoad4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ptrRoad4.Location = new System.Drawing.Point(494, 151);
+            this.ptrRoad4.Name = "ptrRoad4";
+            this.ptrRoad4.Size = new System.Drawing.Size(100, 52);
+            this.ptrRoad4.TabIndex = 7;
+            this.ptrRoad4.TabStop = false;
+            // 
+            // ptrRoad5
+            // 
+            this.ptrRoad5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ptrRoad5.Location = new System.Drawing.Point(658, 151);
+            this.ptrRoad5.Name = "ptrRoad5";
+            this.ptrRoad5.Size = new System.Drawing.Size(100, 52);
+            this.ptrRoad5.TabIndex = 8;
+            this.ptrRoad5.TabStop = false;
+            // 
+            // ptr1
+            // 
+            this.ptr1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ptr1.Location = new System.Drawing.Point(361, 25);
+            this.ptr1.Margin = new System.Windows.Forms.Padding(0);
+            this.ptr1.Name = "ptr1";
+            this.ptr1.Size = new System.Drawing.Size(100, 52);
+            this.ptr1.TabIndex = 10;
+            this.ptr1.TabStop = false;
+            // 
+            // ptr2
+            // 
+            this.ptr2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ptr2.Location = new System.Drawing.Point(614, 25);
+            this.ptr2.Margin = new System.Windows.Forms.Padding(0);
+            this.ptr2.Name = "ptr2";
+            this.ptr2.Size = new System.Drawing.Size(100, 52);
+            this.ptr2.TabIndex = 11;
+            this.ptr2.TabStop = false;
+            // 
+            // ptr3
+            // 
+            this.ptr3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ptr3.Location = new System.Drawing.Point(530, 248);
+            this.ptr3.Margin = new System.Windows.Forms.Padding(0);
+            this.ptr3.Name = "ptr3";
+            this.ptr3.Size = new System.Drawing.Size(100, 52);
+            this.ptr3.TabIndex = 12;
+            this.ptr3.TabStop = false;
+            // 
+            // ptrPlayer
+            // 
+            this.ptrPlayer.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ptrPlayer.Location = new System.Drawing.Point(99, 248);
+            this.ptrPlayer.Margin = new System.Windows.Forms.Padding(0);
+            this.ptrPlayer.Name = "ptrPlayer";
+            this.ptrPlayer.Size = new System.Drawing.Size(100, 52);
+            this.ptrPlayer.TabIndex = 13;
+            this.ptrPlayer.TabStop = false;
+            // 
+            // tmRoad
+            // 
+            this.tmRoad.Interval = 10;
+            this.tmRoad.Tick += new System.EventHandler(this.tmRoad_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(789, 312);
+            this.Controls.Add(this.ptrPlayer);
+            this.Controls.Add(this.ptr3);
+            this.Controls.Add(this.ptr2);
+            this.Controls.Add(this.ptr1);
+            this.Controls.Add(this.ptrRoad5);
+            this.Controls.Add(this.ptrRoad4);
+            this.Controls.Add(this.ptrRoad3);
+            this.Controls.Add(this.ptrRoad2);
+            this.Controls.Add(this.ptrRoad1);
+            this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Speed King";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.ptrRoad1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptrRoad2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptrRoad3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptrRoad4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptrRoad5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptr1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptr2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptr3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptrPlayer)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer tmCar1;
+        private System.Windows.Forms.PictureBox ptrRoad1;
+        private System.Windows.Forms.PictureBox ptrRoad2;
+        private System.Windows.Forms.PictureBox ptrRoad3;
+        private System.Windows.Forms.PictureBox ptrRoad4;
+        private System.Windows.Forms.PictureBox ptrRoad5;
+        private System.Windows.Forms.PictureBox ptr1;
+        private System.Windows.Forms.PictureBox ptr2;
+        private System.Windows.Forms.PictureBox ptr3;
+        private System.Windows.Forms.PictureBox ptrPlayer;
+        private System.Windows.Forms.Timer tmRoad;
+    }
+}
+
---
+++ Version1/Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Version1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int X_Car1, Y_Car1, X_Car2, Y_Car2, X_Car3, Y_Car3, X_Player, Y_Player;
+        int X_Road1, X_Road2, X_Road3, X_Road4, X_Road5;
+        int Y_Road1, Y_Road2, Y_Road3, Y_Road4, Y_Road5;
+        private void tmRoad_Tick(object sender, EventArgs e)
+        {
+            X_Road1 = ptrRoad1.Location.X;
+            Y_Road1 = ptrRoad1.Location.Y;
+            X_Road1 -= 10;
+            if (X_Road1 + ptrRoad1.Size.Width < 0)
+            {
+                X_Road1 = this.Size.Width;
+            }
+            ptrRoad1.Location = new Point(X_Road1, Y_Road1);
+
+            X_Road2 = ptrRoad2.Location.X;
+            Y_Road2 = ptrRoad2.Location.Y;
+            X_Road2 -= 10;
+            if (X_Road2 + ptrRoad2.Size.Width < 0)
+            {
+                X_Road2 = this.Size.Width;
+            }
+            ptrRoad2.Location = new Point(X_Road2, Y_Road2);
+
+            X_Road3 = ptrRoad3.Location.X;
+            Y_Road3 = ptrRoad3.Location.Y;
+            X_Road3 -= 10;
+            if (X_Road3 + ptrRoad3.Size.Width < 0)
+            {
+                X_Road3 = this.Size.Width;
+            }
+            ptrRoad3.Location = new Point(X_Road3, Y_Road3);
+
+            X_Road4 = ptrRoad4.Location.X;
+            Y_Road4 = ptrRoad4.Location.Y;
+            X_Road4 -= 10;
+            if (X_Road4 + ptrRoad4.Size.Width < 0)
+            {
+                X_Road4 = this.Size.Width;
+            }
+            ptrRoad4.Location = new Point(X_Road4, Y_Road4);
+
+            X_Road5 = ptrRoad5.Location.X;
+            Y_Road5 = ptrRoad5.Location.Y;
+            X_Road5 -= 10;
+            if (X_Road5 + ptrRoad5.Size.Width < 0)
+            {
+                X_Road5 = this.Size.Width;
+            }
+            ptrRoad5.Location = new Point(X_Road5, Y_Road5);
+        }
+
+        Random rd1;
+        Random rd2;
+        Random rd3;
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            X_Player = ptrPlayer.Location.X;
+            Y_Player = ptrPlayer.Location.Y;
+            if ((Keys)e.KeyCode == Keys.Up)
+            {
+                
+                if (Y_Player >= 0)
+                {
+                    Y_Player -= 8;
+                    ptrPlayer.Location = new Point(X_Player, Y_Player);
+                }
+            }
+            else if ((Keys)e.KeyCode == Keys.Down)
+            {
+                if (Y_Player + ptrPlayer.Size.Height + 40 <= this.Size.Height)
+                {
+                    Y_Player += 8;
+                    ptrPlayer.Location = new Point(X_Player, Y_Player);
+                }
+            }
+            else if ((Keys)e.KeyCode == Keys.Right)
+            {
+                
+                if (X_Player + ptrPlayer.Size.Width + 23 <= this.Size.Width)
+                {
+                    X_Player += 8;
+                    ptrPlayer.Location = new Point(X_Player, Y_Player);
+                }
+                
+            }
+            else if ((Keys)e.KeyCode == Keys.Left)
+            {
+                if (X_Player - 8 >= 0)
+                {
+                    X_Player -= 8;
+                    ptrPlayer.Location = new Point(X_Player, Y_Player);
+                }
+            }
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.SetStyle(ControlStyles.AllPaintingInWmPaint | ControlStyles.UserPaint | ControlStyles.DoubleBuffer, true);
+            rd1 = new Random();
+            rd2 = new Random();
+            rd3 = new Random();
+
+            this.Size = new Size(800, 350);
+
+            // Hiện xe Player
+            ptrPlayer.Image = Image.FromFile("image/PlayerCar.png");
+            ptrPlayer.SizeMode = PictureBoxSizeMode.StretchImage;
+            ptrPlayer.Size = new Size(80, 25);
+            ptrPlayer.Location = new Point(50, 50);
+
+            tmCar1.Start();
+
+            // Hiện xe chướng ngại vật 1
+            ptr1.Image = Image.FromFile("image/Car1.png");
+            ptr1.SizeMode = PictureBoxSizeMode.StretchImage;
+            ptr1.Size = new Size(80, 30);
+            ptr1.Location = new Point(this.Size.Width - ptr1.Size.Width - 20, 10);
+
+            // Hiện xe chướng ngại vật 2
+            ptr2.Image = Image.FromFile("image/Car2.png");
+            ptr2.SizeMode = PictureBoxSizeMode.StretchImage;
+            ptr2.Size = new Size(80, 25);
+            ptr2.Location = new Point(this.Size.Width - ptr1.Size.Width - 20, ptr1.Location.Y + ptr1.Size.Height + 30);
+
+            // Hiện xe chướng ngại vật 3
+            ptr3.Image = Image.FromFile("image/Car3.png");
+            ptr3.SizeMode = PictureBoxSizeMode.StretchImage;
+            ptr3.Size = new Size(80, 25);
+            ptr3.Location = new Point(this.Size.Width - ptr1.Size.Width - 20, ptr2.Location.Y + ptr2.Size.Height + 30);
+
+            // Hiện Lằn đường
+            ptrRoad1.Image = Image.FromFile("image/road.png");
+            ptrRoad1.SizeMode = PictureBoxSizeMode.StretchImage;
+            ptrRoad1.Size = new Size(80, 10);
+
+            ptrRoad2.Image = Image.FromFile("image/road.png");
+            ptrRoad2.SizeMode = PictureBoxSizeMode.StretchImage;
+            ptrRoad2.Size = new Size(80, 10);
+
+            ptrRoad3.Image = Image.FromFile("image/road.png");
+            ptrRoad3.SizeMode = PictureBoxSizeMode.StretchImage;
+            ptrRoad3.Size = new Size(80, 10);
+
+            ptrRoad4.Image = Image.FromFile("image/road.png");
+            ptrRoad4.SizeMode = PictureBoxSizeMode.StretchImage;
+            ptrRoad4.Size = new Size(80, 10);
+
+            ptrRoad5.Image = Image.FromFile("image/road.png");
+            ptrRoad5.SizeMode = PictureBoxSizeMode.StretchImage;
+            ptrRoad5.Size = new Size(80, 10);
+
+            tmRoad.Start();
+
+        }
+
+        private void tmCar1_Tick(object sender, EventArgs e)
+        {
+            this.SetStyle(ControlStyles.AllPaintingInWmPaint | ControlStyles.UserPaint | ControlStyles.DoubleBuffer, true);
+            
+
+            Y_Car1 = ptr1.Location.Y;
+            X_Car1 = ptr1.Location.X;
+            X_Car1 -= 4;
+            if (X_Car1 + ptr1.Size.Width < 0)
+            {
+                X_Car1 = this.Size.Width + ptr1.Size.Width;
+                Y_Car1 = rd1.Next(0, this.Size.Height - ptr1.Size.Height - 30);
+            }
+            ptr1.Location = new Point(X_Car1, Y_Car1);
+
+            Y_Car2 = ptr2.Location.Y;
+            X_Car2 = ptr2.Location.X;
+            X_Car2 -= 6;
+            if (X_Car2 + ptr2.Size.Width < 0)
+            {
+                X_Car2 = this.Size.Width + ptr2.Size.Width;
+                Y_Car2 = rd2.Next(0, this.Size.Height - ptr1.Size.Height - 30);
+            }
+            ptr2.Location = new Point(X_Car2, Y_Car2);
+
+            Y_Car3 = ptr3.Location.Y;
+            X_Car3 = ptr3.Location.X;
+            X_Car3 -= 8;
+            if (X_Car3 + ptr3.Size.Width < 0)
+            {
+                X_Car3 = this.Size.Width + ptr3.Size.Width;
+                Y_Car3 = rd3.Next(0, this.Size.Height - ptr1.Size.Height - 30);
+            }
+            ptr3.Location = new Point(X_Car3, Y_Car3);
+
+            // Kiểm tra đụng độ với xe 1
+            if (X_Car1 >= X_Player && X_Car1 <= X_Player + ptrPlayer.Size.Width && Y_Player >= Y_Car1 && Y_Player <= Y_Car1 + ptr1.Size.Height || Y_Car1 >= Y_Player && Y_Car1 <= Y_Player + ptrPlayer.Size.Height && X_Car1 >= X_Player && X_Car1 <= X_Player + ptrPlayer.Size.Width || X_Player >= X_Car1 && X_Player <= X_Car1 + ptr1.Size.Width && Y_Player >= Y_Car1 && Y_Player <= Y_Car1 + ptr1.Size.Height || X_Player >= X_Car1 && X_Player <= X_Car1 + ptr1.Size.Width && Y_Car1 >= Y_Player && Y_Car1 <= Y_Player + ptrPlayer.Size.Height)
+            {
+                tmCar1.Stop();
+                tmRoad.Stop();
+                MessageBox.Show("Bùm");
+            }
+
+            // Kiểm tra đụng độ với xe 2
+            if (X_Car2 >= X_Player && X_Car2 <= X_Player + ptrPlayer.Size.Width && Y_Player >= Y_Car2 && Y_Player <= Y_Car2 + ptr2.Size.Height || Y_Car2 >= Y_Player && Y_Car2 <= Y_Player + ptrPlayer.Size.Height && X_Car2 >= X_Player && X_Car2 <= X_Player + ptrPlayer.Size.Width || X_Player >= X_Car2 && X_Player <= X_Car2 + ptr2.Size.Width && Y_Player >= Y_Car2 && Y_Player <= Y_Car2 + ptr2.Size.Height || X_Player >= X_Car2 && X_Player <= X_Car2 + ptr2.Size.Width && Y_Car2 >= Y_Player && Y_Car2 <= Y_Player + ptrPlayer.Size.Height)
+            {
+                tmCar1.Stop();
+                tmRoad.Stop();
+                MessageBox.Show("Bùm");
+            }
+
+            // Kiểm tra đụng độ với xe 3
+            if (X_Car3 >= X_Player && X_Car3 <= X_Player + ptrPlayer.Size.Width && Y_Player >= Y_Car3 && Y_Player <= Y_Car3 + ptr3.Size.Height || Y_Car3 >= Y_Player && Y_Car3 <= Y_Player + ptrPlayer.Size.Height && X_Car3 >= X_Player && X_Car3 <= X_Player + ptrPlayer.Size.Width || X_Player >= X_Car3 && X_Player <= X_Car3 + ptr3.Size.Width && Y_Player >= Y_Car3 && Y_Player <= Y_Car3 + ptr3.Size.Height || X_Player >= X_Car3 && X_Player <= X_Car3 + ptr3.Size.Width && Y_Car3 >= Y_Player && Y_Car3 <= Y_Player + ptrPlayer.Size.Height)
+            {
+                tmCar1.Stop();
+                tmRoad.Stop();
+                MessageBox.Show("Bùm");
+            }
+
+        }
+    }
+}
